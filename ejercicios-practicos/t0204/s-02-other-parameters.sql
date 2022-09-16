155e0caeb475f36ca1a1f5a35f44f0d604820af6
kiliapplered <kiliapplered@hotmail.com>
2022-09-16 18:08:53 -0500

    kestapasanda

--- ejercicios-practicos/t0204/s-02-other-parameters.sql
+++ ejercicios-practicos/t0204/s-02-other-parameters.sql
@@ -8,13 +8,13 @@ Prompt Accediendo como usuario sys
 connect sys/system2 as sysdba
 
 Prompt Creando tabla de parámetros de la BD
-create table karla0204.t02_other_parameters as
+create table karla0204.t_02_other_parameters as
   (select num, name, value, default_value, isses_modifiable as is_session_modifiable, 
   issys_modifiable as is_system_modifiable 
     from v$system_parameter);
 
 Prompt Mostrando tabla;
 set linesize window;
-select * from karla0204.t02_other_parameters;
+select * from karla0204.t_02_other_parameters;
 
 whenever sqlerror continue none;
