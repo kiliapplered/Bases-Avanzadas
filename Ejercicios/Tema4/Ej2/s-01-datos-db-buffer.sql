123020e4c302ed82756ab4553ddb10eadb360a2b
Karla Najera <kiliapplered@hotmail.com>
2022-09-27 20:45:03 -0500

    Washa

--- Ejercicios/Tema4/Ej2/s-01-datos-db-buffer.sql
+++ Ejercicios/Tema4/Ej2/s-01-datos-db-buffer.sql
@@ -7,8 +7,31 @@ whenever sqlerror exit rollback;
 Prompt Conectando como sysdba
 connect sys/system2 as sysdba
 
+prompt Creando tabla t01_db_buffer_cache
+create table froylan0402.t01_db_buffer_cache as(
+  (select block_size,current_size,buffers,
+    target_buffers,prev_size,prev_buffers,
+    0 default_pool_size
+  from v$buffer_pool)
+);
 
-
-commit;
+prompt Creando tabla t02_db_buffer_sysstats
+create table froylan0402.t02_db_buffer_sysstats as(
+  select q1.db_blocks_gets_from_cache,
+    q2.consistent_gets_from_cache,
+    q3.physical_reads_cache,
+    trunc(1-(q3.physical_reads_cache/(q1.db_blocks_gets_from_cache+q2.consistent_gets_from_cache)),6)
+      cache_hit_radio
+  from
+    (select value as db_blocks_gets_from_cache
+    from v$sysstat
+    where name='db block gets from cache') q1,
+    (select value as consistent_gets_from_cache
+    from v$sysstat
+    where name='consistent gets from cache') q2,
+    (select value as physical_reads_cache
+    from v$sysstat
+    where name='physical reads cache') q3
+);
 
 whenever sqlerror continue none;
