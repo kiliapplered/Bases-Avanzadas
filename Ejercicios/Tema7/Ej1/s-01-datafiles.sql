a6da0cad81fc57d6daf425ad77cecd4136844c37
kiliapplered <kiliapplered@hotmail.com>
2022-12-27 08:03:51 -0600

    Ejercicio 1 tema 7 terminado

--- Ejercicios/Tema7/Ej1/s-01-datafiles.sql
+++ Ejercicios/Tema7/Ej1/s-01-datafiles.sql
@@ -4,40 +4,52 @@
 
 whenever sqlerror exit rollback;
 
+set linesize window
+
 Prompt Conectando como usuario sysdba
-connect sysdba/system2
+connect sys/system2 as sysdba
 
--- Inciso A versión 1
-select file#, name, con_id, round(bytes/1024/1024, 2) tam_mb from v$datafile where name like '%store_tbs_multiple%';
+set linesize window
+column nombre_df format a30
 
--- Inciso A versión 2
-select file_name, file_id, relative_fno, round(bytes/1024/1024, 2) from dba_data_files where tablespace_name='store_tbs_multiple';
+-- Inciso A
+Prompt Consulta 1
+col file_name format a65
+select file_name, file_id, round(bytes/1024/1024, 2) tam_mb
+from dba_data_files where tablespace_name='STORE_TBS_MULTIPLE';
 
 --- Inciso B
+Prompt Consulta 2
 select 
-  tablespace_name, round(sum(bytes)/1024/1024, 2) mb_free, sum(blocks)
+  tablespace_name, round(sum(bytes)/1024/1024, 2) mb_free, sum(blocks) bloques_disponibles
 from dba_free_space
-where tablespace_name='store_tbs_multiple'
+where tablespace_name='STORE_TBS_MULTIPLE'
 group by tablespace_name;
 
 -- Inciso C
-Prompt creando al usuario karla072 y asignando privilegios
+Prompt Creando al usuario karla072 y asignando privilegios
+drop user karla072 cascade;
 create user karla072 identified by karla quota unlimited on store_tbs_multiple;
 alter user karla072 default tablespace store_tbs_multiple;
+grant create session, create table, create procedure to karla072;
 
 -- Inciso D
+Prompt Creando tabla karla_tbs_multiple
 create table karla072.karla_tbs_multiple (
-  str  varchar2(1000);
-)
-segment creation immediate;
+  str  char(1024)
+) segment creation immediate;
 
 -- Inciso E
--- Pendiente
+Prompt Consulta 3
+select df.file_name nombre_df, df.file_id id_df,
+  s.extents total_extents, s.bytes/1024/1024 total_mbs, s.blocks total_bloques
+from dba_data_files df join dba_segments s on s.header_file=df.file_id
+where s.segment_name='KARLA_TBS_MULTIPLE';
 
 -- Inciso F
 Prompt Bloque anónimo para poblar la tabla con 512 registros
 declare
-  v_str varchar2(1000);
+  v_str varchar2(1024);
 begin
   v_str:='Llenando la tabla con cadenas random';
   for v_index in 1..512 loop
@@ -47,14 +59,20 @@ begin
   commit;
 end;
 /
+show errors
+commit;
 
 -- Inciso G
--- Pendiente
+Prompt Consulta del inciso E otra vez 
+select df.file_name nombre_df, df.file_id id_df,
+  s.extents total_extents, s.bytes/1024/1024 total_mbs, s.blocks total_bloques
+from dba_data_files df join dba_segments s on s.header_file=df.file_id
+where s.segment_name='KARLA_TBS_MULTIPLE';
 
 -- Inciso H
 Prompt Bloque anónimo para poblar la tabla con 2560 registros
 declare
-  v_str varchar2(1000);
+  v_str varchar2(1024);
 begin
   v_str:='Llenando la tabla con cadenas random';
   for v_index in 1..2560 loop
@@ -64,12 +82,22 @@ begin
   commit;
 end;
 /
+show errors
+commit;
+
+-- Inciso E de nuevo
+Prompt Consulta del inciso E otra vez 
+select df.file_name nombre_df, df.file_id id_df,
+  s.extents total_extents, s.bytes/1024/1024 total_mbs, s.blocks total_bloques
+from dba_data_files df join dba_segments s on s.header_file=df.file_id
+where s.segment_name='KARLA_TBS_MULTIPLE';
 
 -- Inciso I
+Prompt Consulta  del inciso B otra vez
 select 
-  tablespace_name, round(sum(bytes)/1024/1024, 2) mb_free, sum(blocks)
+  tablespace_name, round(sum(bytes)/1024/1024, 2) mb_free, sum(blocks) bloques_disponibles
 from dba_free_space
-where tablespace_name='store_tbs_multiple'
+where tablespace_name='STORE_TBS_MULTIPLE'
 group by tablespace_name;
 
 whenever sqlerror continue none;
